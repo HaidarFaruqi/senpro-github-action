e32c9193b34a18afff037e0dc2190eec1ea60a00
HaidarFaruqi <haidarfaruqi126@gmail.com>
2026-02-24 10:37:39 +0700

    index.js comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Haidar Faruqi Al Ghifari | 528252
           </h5>
         </div>
       </main>
